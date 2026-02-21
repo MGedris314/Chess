e45f0c45fdfaf9faa22b6efd755e2577ba0556a7
Matthew Gedris <mdgedris@byu.edu>
2026-02-21 15:53:36 -0700

    Added endpoints, though we're getting an error message regarding .post and friends not being accepted as functions in server.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -8,3 +8,6 @@ public class Main {
         System.out.println("♕ 240 Chess Server");
     }
 }
+/*
+
+ */
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,51 @@
 package server;
 
 import io.javalin.*;
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.Javalin;
+
 
 public class Server {
 
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
+        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        .post("/user", this::addUser);
+        .post("/session", this::logIn);
+        .post("/game", this::createGame);
+        .put("/game", this::joinGame);
+        .get("game", this::listGames);
+        .delete("/session", this::logOut);
+        .delete("/db", this::fullClear);
         // Register your endpoints and exception handlers here.
 
+
+    }
+
+
+    private void addUser(Context ctx){
+        int x = 0;
+    }
+    private void logIn(Context ctx){
+        int x = 0;
+    }
+    private void createGame(Context ctx){
+        int x = 0;
+    }
+    private void joinGame(Context ctx){
+        int x = 0;
+    }
+    private void listGames(Context ctx){
+        int x = 0;
+    }
+    private void logOut(Context ctx){
+        int x = 0;
+    }
+    private void fullClear(Context ctx){
+        int x = 0;
     }
 
     public int run(int desiredPort) {
