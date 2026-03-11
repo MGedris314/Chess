d09daa8aab18f059d654d1365c63eb0c57ccfdd8
Matthew Gedris <mdgedris@byu.edu>
2026-03-10 22:17:24 -0600

    I should have everything set up to the point where I can start connecting to the database and seeing if my code does what I think it should do.

--- server/src/main/java/dataaccess/SQLDataAccess.java
+++ server/src/main/java/dataaccess/SQLDataAccess.java
@@ -1,10 +1,14 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import exception.UserException403;
 import model.*;
 
 import javax.xml.crypto.Data;
 import java.sql.*;
+import java.util.ArrayList;
+
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;;
 
@@ -84,9 +88,11 @@ public class SQLDataAccess implements DataAccess {
     @Override
     public AuthData findAuth(String authData) {
         try(var con = DatabaseManager.getConnection()) {
-            try (var statement = con.prepareStatement( "SELECT * FROM users WHERE token = authData")){
+            try (var statement = con.prepareStatement( "SELECT name FROM users WHERE token = authData")){
                 try(var rs = statement.executeQuery()){
-
+                    statement.setString(1, authData);
+                    var authenticated = rs.getString("name");
+                    AuthData auth = new AuthData(authenticated, authData);
                     return null;
                 }
             }
@@ -99,62 +105,210 @@ public class SQLDataAccess implements DataAccess {
 
     @Override
     public void removeAuth(String authData) {
-        var statement = "UPDATE users SET token = NULL WHERE token = authData";
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "UPDATE users SET token = 0 WHERE token = authData")){
+                try(var rs = statement.executeQuery()){
+                    statement.setString(1, authData);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int createGame(GameData gameName) {
-        var statement = "INSERT INTO public (id, name, whiteTeam, blackTeam) VALUES(?, ?, null, null, ?";
-        return 0;
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "INSERT INTO games (id, name, whiteTeam, blackTeam, game) VALUES(?, ?, null, null, ?")){
+                try(var rs = statement.executeQuery()){
+                    var state = gameName.game();
+                    var serializer = new Gson();
+                    var json = serializer.toJson(state);
+                    statement.setString(2, gameName.gameName());
+                    statement.setString(5, json);
+                    var ID = rs.getInt("id");
+                    return ID;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void createPublic(PublicGame pub) {
-        var statement = "INSERT INTO public (id, name, whiteTeam, blackTeam) VALUES(?, ?, null, null, ?";
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "INSERT INTO public (id, name, whiteTeam, blackTeam) VALUES(?, ?, null, null, ?")){
+                try(var rs = statement.executeQuery()){
+                    statement.setString(2, pub.gameName());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public int gameID() {
-        return 0;
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "SELECT * FROM games WHERE id = (SELECT MAX (id) FROM games)")){
+                try(var rs = statement.executeQuery()){
+                    var id = rs.getInt("id");
+                    return id;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public GameRetrun gameReturn() {
-        var statement = "SELECT * FROM public";
-        return null;
+        ArrayList<PublicGame> returnable = new ArrayList<PublicGame>();
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "SELECT * FROM public")){
+                try(var rs = statement.executeQuery()){
+                    while(rs.next()) {
+                        var name = rs.getString("name");
+                        var wTeam = rs.getString("whiteTeam");
+                        var bTeam = rs.getString("blackTeam");
+                        var id = rs.getInt("id");
+                        PublicGame pub = new PublicGame(id,name, wTeam, bTeam);
+                        returnable.add(pub);
+                    }
+                    GameRetrun games = new GameRetrun(returnable);
+                    return games;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public GameData returnSingleGame(int gameID) throws UserException403 {
-        var statement = "SELECT * FROM games WHERE id = gameID";
-        return null;
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "SELECT * FROM games WHERE id = gameID")){
+                try(var rs = statement.executeQuery()){
+                    var name = rs.getString("name");
+                    var wTeam = rs.getString("whiteTeam");
+                    var bTeam = rs.getString("blackTeam");
+                    var id = rs.getInt("id");
+                    var json = rs.getString("game");
+                    var serializer = new Gson();
+                    var fromJson = serializer.fromJson(json, ChessGame.class);
+                    GameData game = new GameData(id, wTeam, bTeam, name, fromJson);
+                    return game;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public PublicGame editPublic(int gameID) {
-        var statement = "SELECT * FROM public WHERE id = gameID";
-        return null;
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "SELECT * FROM public WHERE id = gameID")){
+                try(var rs = statement.executeQuery()){
+                    statement.setInt(1, gameID);
+                    var name = rs.getString("name");
+                    var wTeam = rs.getString("whiteTeam");
+                    var bTeam = rs.getString("blackTeam");
+                    var id = rs.getInt("id");
+                    PublicGame pub = new PublicGame(id, wTeam, bTeam, name);
+                    return pub;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void updateGames(GameData game, PublicGame pub, int gameID) {
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "UPDATE games SET whiteTeam = ? blackTeam = ? WHERE id = gameID")){
+                try(var rs = statement.executeQuery()){
+                    statement.setInt(3, gameID);
+                    statement.setString(2, game.blackUsername());
+                    statement.setString(1, game.whiteUsername());
+                }
+            }
+            try (var statement = con.prepareStatement( "UPDATE public SET whiteTeam = ? blackTeam = ? WHERE id = gameID")){
+                try(var rs = statement.executeQuery()){
+                    statement.setInt(3, gameID);
+                    statement.setString(2, game.blackUsername());
+                    statement.setString(1, game.whiteUsername());
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
         var statement1 = "UPDATE games SET whiteTeam = ? blackTeam = ? WHERE id = gameID";
         var statement2 = "UPDATE public SET whiteTeam = ? blackTeam = ? WHERE id = gameID";
     }
 
     @Override
     public void clearAuth() {
-        var statement = "DROP COLUMN token";
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "DROP COLUMN token")){
+                try(var rs = statement.executeQuery()){
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void clearUsers() {
-        var statement = "DELETE FROM users WHERE id=?";
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "DROP TABLE users")){
+                try(var rs = statement.executeQuery()){
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void clearGames() {
-        var statement = "DELETE FROM games WHERE id=?";
+        try(var con = DatabaseManager.getConnection()) {
+            try (var statement = con.prepareStatement( "DROP TABLE games")){
+                try(var rs = statement.executeQuery()){
+                }
+            }
+            try (var statement = con.prepareStatement( "DROP TABLE public")){
+                try(var rs = statement.executeQuery()){
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private final String [] createUserStatements = {
