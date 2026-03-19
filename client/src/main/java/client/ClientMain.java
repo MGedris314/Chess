9c329de68ddf64003c4b58d7e2cafac9b4b14328
Matthew Gedris <mdgedris@byu.edu>
2026-03-18 20:44:03 -0600

    Menu object 1 is done, need to figure out menu two.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -6,7 +6,6 @@ import chess.*;
 import java.util.Scanner;
 
 public class ClientMain {
-
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
@@ -16,10 +15,9 @@ public class ClientMain {
 
     public static void runDefault(){
 //        System.out.println("Hey this works");
-        int state = 0;
         Scanner scanner = new Scanner(System.in);
         String responce = "";
-        System.out.println(help(state));
+        System.out.println(help1());
         while(!responce.equals("quit")){
             responce = scanner.nextLine();
             input1(responce);
@@ -31,8 +29,15 @@ public class ClientMain {
         switch (check){
             case "log in" -> logIn();
             case "quit" -> escape();
+            case "help" -> help1();
+            case "register" -> register();
+            case "burnt chicken" -> secrets();
             default-> zeroedOut();
         }
+    }
+
+    private static void secrets(){
+        System.out.println("Ahh, I see you are trying to discover secrets.  Come back at a later time.....");
     }
 
     private static String zeroedOut(){
@@ -44,7 +49,9 @@ public class ClientMain {
         return 0;
     }
 
-    public static void runLevel2(){}
+    public static void runLevel2(){
+        System.out.println("Inside level 2");
+    }
 
     private static String logIn(){
         Scanner log = new Scanner(System.in);
@@ -56,6 +63,7 @@ public class ClientMain {
         pass = log.nextLine();
         if(user.equals("Steve") && pass.equals("a")){
             System.out.println("Logged in");
+            runLevel2();
             return "Logged in";
         }
         else{
@@ -64,23 +72,27 @@ public class ClientMain {
         }
     }
 
-
-    public static String help(int state){
-        if (state == 0){
-            return """
-                    Optional commands for this window:
-                    Help: returns this menu
-                    Log in: If you are registered as an existing user log in with username and password credentials
-                    Register:  If you do not have an existing account create one
-                    Quit:  Leave program.
-                    
-                    What would you like to do?
-                    """;
-        }
-        else{
-            return "Work in progress.";
-        }
+    private static String register() {
+        Scanner log = new Scanner(System.in);
+        String user = "";
+        String pass = "";
+        System.out.println("Enter your user name: ");
+        user = log.nextLine();
+        System.out.println("Enter your password: ");
+        pass = log.nextLine();
+        runLevel2();
+        return "Registered";
     }
 
-
-}
+    public static String help1(){
+        return """
+            Optional commands for this window:
+            Help: returns this menu
+            Log in: If you are registered as an existing user log in with username and password credentials
+            Register:  If you do not have an existing account create one
+            Quit:  Leave program.
+            
+            What would you like to do?
+            """;
+        }
+    }
