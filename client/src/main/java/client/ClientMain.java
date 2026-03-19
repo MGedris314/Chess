421cd53d38e973b89c0a8d1968676de7a2013276
Matthew Gedris <mdgedris@byu.edu>
2026-03-18 20:20:54 -0600

    Log in and quit work with controlled data.

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -2,6 +2,9 @@ package client;
 
 import chess.*;
 
+
+import java.util.Scanner;
+
 public class ClientMain {
 
     public static void main(String[] args) {
@@ -14,10 +17,53 @@ public class ClientMain {
     public static void runDefault(){
 //        System.out.println("Hey this works");
         int state = 0;
+        Scanner scanner = new Scanner(System.in);
+        String responce = "";
         System.out.println(help(state));
+        while(!responce.equals("quit")){
+            responce = scanner.nextLine();
+            input1(responce);
+        }
+    }
+
+    public static void input1(String req){
+        String check = req.toLowerCase();
+        switch (check){
+            case "log in" -> logIn();
+            case "quit" -> escape();
+            default-> zeroedOut();
+        }
+    }
+
+    private static String zeroedOut(){
+        System.out.println("Hmmm..... It looks like you entered a value that isn't there.");
+        return "a";
+    }
+
+    private static int escape(){
+        return 0;
     }
 
     public static void runLevel2(){}
+
+    private static String logIn(){
+        Scanner log = new Scanner(System.in);
+        String user = "";
+        String pass = "";
+        System.out.println("Enter your user name: ");
+        user = log.nextLine();
+        System.out.println("Enter your password: ");
+        pass = log.nextLine();
+        if(user.equals("Steve") && pass.equals("a")){
+            System.out.println("Logged in");
+            return "Logged in";
+        }
+        else{
+            System.out.println("Incorrect log in credentials.");
+            return "Invalid log inn credentials";
+        }
+    }
+
 
     public static String help(int state){
         if (state == 0){
@@ -27,6 +73,8 @@ public class ClientMain {
                     Log in: If you are registered as an existing user log in with username and password credentials
                     Register:  If you do not have an existing account create one
                     Quit:  Leave program.
+                    
+                    What would you like to do?
                     """;
         }
         else{
--- client/src/main/java/ui/boardDraw.java
+++ client/src/main/java/ui/boardDraw.java
@@ -2,7 +2,6 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 import static ui.EscapeSequences.*;
 
