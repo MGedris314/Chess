2938a108f6c9cac59910e790c66903419207e54a
Matthew Gedris <mdgedris@byu.edu>
2026-04-07 14:44:02 -0600

    Normal resign works along with two other tests, kind of.

--- server/src/main/java/websocket/WebsocketHandler.java
+++ server/src/main/java/websocket/WebsocketHandler.java
@@ -28,6 +28,8 @@ import java.util.HashMap;
 public class WebsocketHandler implements WsConnectHandler, WsMessageHandler {
     HashMap<String, Integer> sessionInfo = new HashMap<>();
     HashMap<String, Session> sessions = new HashMap<>();
+    HashMap<Integer, Boolean> gameLog = new HashMap<>();
+//  If the boolean is true in game log, the game is open, if it's false the game is closed(finished).
 
     @Override
     public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
@@ -40,9 +42,9 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler {
         UserGameCommand command = new Gson().fromJson(wsMessageContext.message(), UserGameCommand.class);
         switch (command.getCommandType()){
             case CONNECT -> connect(wsMessageContext, command);
-            case MAKE_MOVE -> move();
+            case MAKE_MOVE -> move(wsMessageContext, command);
             case LEAVE -> leave();
-            case RESIGN -> resign();
+            case RESIGN -> resign(wsMessageContext, command);
         }
     }
     public int connect(WsMessageContext context, UserGameCommand command){
@@ -52,11 +54,11 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler {
         try{
             AuthData name = service.findUser(command.getAuthToken());
             GameData trial = service.observe(id);
-            if(trial == null){
+            if(trial == null || name == null){
                 System.out.println("We need to error out here");
                 ErrorMessage errored = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "ERROR");
                 String msg = new Gson().toJson(errored);
-                sender(name.userName(), msg, context, 0,-1);
+                sender(null, msg, context, 0,-1);
                 return -1;
             }else {
                 LoadGameMessage load = new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, trial);
@@ -64,6 +66,7 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler {
                 context.session.getRemote().sendString(serialzed);
                 sessionInfo.put(name.userName(), id);
                 sessions.put(name.userName(), context.session);
+                gameLog.put(id, true);
                 NotificationMessages note = new NotificationMessages(ServerMessage.ServerMessageType.NOTIFICATION, "Has joined the game");
                 String msg = new Gson().toJson(note);
                 sender(name.userName(), msg, context, id,0);
@@ -75,9 +78,51 @@ public class WebsocketHandler implements WsConnectHandler, WsMessageHandler {
             return 2;
         }
     }
-    public void move(){}
+    public void move(WsMessageContext context, UserGameCommand command){
+        int id = command.getGameID();
+        DataAccess access = new SQLDataAccess();
+        GameService service = new GameService(access);
+        try {
+            AuthData name = service.findUser(command.getAuthToken());
+            GameData trial = service.observe(id);
+            if(trial == null || name == null){
+                System.out.println("We need to error out here");
+                ErrorMessage errored = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "ERROR");
+                String msg = new Gson().toJson(errored);
+                sender(null, msg, context, 0,-1);
+            }else {
+                System.out.println("We'll fix this later");
+            }
+        } catch (UserExceptions e) {
+            System.out.println("Errored");
+        } catch (IOException e) {
+            System.out.println("Errored again.");
+        }
+    }
     public void leave(){}
-    public void resign(){}
+    public void resign(WsMessageContext context, UserGameCommand command){
+        int id = command.getGameID();
+        if(gameLog.get(id) == true){
+            gameLog.replace(id, true, false);
+            NotificationMessages note = new NotificationMessages(ServerMessage.ServerMessageType.NOTIFICATION, "has resigned");
+            String msg = new Gson().toJson(note);
+            try{
+                sender(null, msg, context, id, 0);
+            }
+            catch (IOException e){
+                System.out.println("Something has gone wrong sending the message");
+            }
+        }
+        else{
+            ErrorMessage errored = new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "The game is over, why are you resigning?");
+            String msg = new Gson().toJson(errored);try{
+                sender(null, msg, context, id, -1);
+            }
+            catch (IOException e){
+                System.out.println("Something has gone wrong sending the message");
+            }
+        }
+    }
 
     public void sender (String exclude, String notification, WsMessageContext context, int id, int e) throws IOException {
         if(e == -1){
